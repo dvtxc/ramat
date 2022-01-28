46f4b001ed580301c760b297da3be49ec0102872
Dmitri Visser <dmitri95dwt@gmail.com>
2022-01-28 13:07:48 +0100

    auto install of witio toolbox

--- src/ramat/helper/enableWITIOhelper.m
+++ src/ramat/helper/enableWITIOhelper.m
@@ -1,12 +1,58 @@
-function enableWITIOhelper()
+function success = enableWITIOhelper(kwargs)
+    % ENABLEWITIOHELPER Enables the witio toolbox
 
-if witio_isenabled()
-    % Check if helper functions can be found
-    if ~exist('varargin_dashed_str_inds')     
-        [dir, ~, ~] = fileparts(which('wit_io_license'));
-        helperdir = [dir, '\helper'];
-        addpath(genpath(helperdir));
+    arguments
+        kwargs.action = "add_helper";
     end
-end
+
+    success = false;
+
+    switch kwargs.action
+        case 'install'
+            install_witio_toolbox();
+        case 'add_helper'
+            add_witio_helper_functions();
+    end
+
+
+    function install_witio_toolbox()
+        % INSTALL_WITIO_TOOLBOX Run the witio toolbox installation
+
+        toolbox_path = fullfile('redist', 'wit_io');
+
+        if ~exist(fullfile(toolbox_path, 'dir'))
+            fprintf('Toolbox installation file not found.\n');
+            return
+        end
+
+        try
+            installedToolbox = matlab.addons.toolbox.installToolbox( fullfile(toolbox_path, 'wit_io.mltbx') );
+        catch ME
+            % Repackage error as warning, continue without toolbox
+            warning(ME.message);
+            return
+        end
+
+        fprintf('Installed WITIO Toolbox.\n');
+        success = true;
+
+    end
+
+    function add_witio_helper_functions()
+        % ADD_WITIO_HELPER_FUNCTIONS Adds /helper subdir of witio toolbox
+
+        if ~exist('varargin_dashed_str_inds')
+            % Get root dir of witio toolbox
+            [dir, ~, ~] = fileparts(which('wit_io_license'));
+
+            % Add helper content
+            helperdir = fullfile(dir, 'helper');
+            addpath(genpath(helperdir));
+        end
+
+        fprintf('Added WITIO helper functions to MATLAB path.\n');
+        success = true;
+
+    end
 
 end
--- src/ramat/helper/witio_isenabled.m
+++ src/ramat/helper/witio_isenabled.m
@@ -1,20 +1,85 @@
 function [status, identifier] = witio_isenabled()
-% Checks whether the WITIO reader is enabled and all helper functions can
-% be found.
+    % WITIO_ISENABLED Checks whether the witio add on is enabled
+    %   Checks whether the WITIO reader is enabled and all helper functions can
+    %   be found. When the reader is not found, ask the user to install and
+    %   enable the extension.
+    %
+    %   WITIO_ISENABLED() performs check and asks user
+    %
+    %   See also ENABLEWITIOHELPER.
 
-    installedAddons = matlab.addons.installedAddons;
-    witio_row = installedAddons(ismember(installedAddons.Name,'wit_io'),:);
 
+    % default output
     identifier = -1;
     status = false;
 
-    if ~isempty(witio_row)
-        % WITIO module is installed
-        witio_row = witio_row(1,:);
+    % Get list of installed add ons
+    installedAddons = matlab.addons.installedAddons;
 
-        identifier = witio_row.Identifier;   
-        status = matlab.addons.isAddonEnabled(identifier);
+    % Check for witio toolbox
+    witio_row = installedAddons(ismember(installedAddons.Name,'wit_io'),:); 
 
+    if isempty(witio_row)
+        fprintf('WITIO Toolbox is not installed.\n');
+
+        if yesno_prompt('Do you want to install WITIO Toolbox?')
+            enableWITIOhelper(action='install');
+        else
+            return
+        end
+
+    end
+
+    fprintf('WITIO toolbox found.\n')
+
+    % WITIO module is installed
+    witio_row = witio_row(1,:);
+    identifier = witio_row.Identifier;   
+    status = matlab.addons.isAddonEnabled(identifier);
+
+    if ~status
+        fprintf('The WITIO reader is not enabled. This is required to read WITec project files.\n');
+        
+        if yesno_prompt('Do you want to enable the WITIO Toolbox?')
+            
+            % Enable the toolbox
+            try
+                matlab.addons.enableAddon(identifier)
+            catch ME
+                warning(ME.message);
+                return
+            end
+
+        else
+            return
+        end
+    end
+
+    fprintf('WITIO toolbox enabled.\n')
+
+    if ~exist('varargin_dashed_str_inds')
+        fprintf('WITIO toolbox helper functions are not loaded.\n');
+        
+        enableWITIOhelper(action="add_helper");
+    end
+
+    % end --
+
+    function out = yesno_prompt(question)
+        prompt = sprintf('%s Options: [Y/YES, n/no]\n', question);
+        validResponse = false;
+        
+        while ~validResponse
+            response = lower(input(prompt,'s'));
+            validResponse = ismember(response,{'yes','y','no','n',''});
+        end
+
+        % Determine which response was entered
+        if strcmpi(response,'yes') || strcmpi(response,'y') || isempty(response)
+            out = true;
+        else
+            out = false;
+        end
     end
 
 end
